2323a8612bbca48ba752f31624f568fcffe9e2e0
necromfox <necromfox@gmail.com>
2022-01-29 14:58:49 +0300

    add namespace to AsteroidSystem

--- heat-range/scripts/AsteroidSystem.cs
+++ heat-range/scripts/AsteroidSystem.cs
@@ -3,14 +3,17 @@ using System;
 using System.Collections.Generic;
 
 
-public static class AsteroidSystem
+namespace HeatRange
 {
-    public static List<RigidBody2D> Asteroids { get; set; } = new();
+    public static class AsteroidSystem
+    {
+        public static List<RigidBody2D> Asteroids { get; set; } = new();
 
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+        //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+        //  public override void _Process(float delta)
+        //  {
+        //      
+        //  }
+    }
 }
