e998a88ffc7973c4699e9f4e66ffcb9cac4f1d68
Dest0re <klacik2002@yandex.ru>
2022-01-30 07:05:20 +0500

    Add Reactor texture

--- heat-range/scripts/AsteroidSystem.cs
+++ heat-range/scripts/AsteroidSystem.cs
@@ -5,15 +5,15 @@ using System.Collections.Generic;
 
 namespace HeatRange
 {
-    public static class AsteroidSystem
-    {
-        public static List<RigidBody2D> Asteroids { get; set; } = new();
+	public static class AsteroidSystem
+	{
+		public static List<RigidBody2D> Asteroids { get; set; } = new();
 
 
-        //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-        //  public override void _Process(float delta)
-        //  {
-        //      
-        //  }
-    }
+		//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+		//  public override void _Process(float delta)
+		//  {
+		//      
+		//  }
+	}
 }
--- heat-range/scripts/def/Def.cs
+++ heat-range/scripts/def/Def.cs
@@ -1,24 +1,24 @@
 namespace HeatRange
 {
-    public static partial class Def
-    {
-        public static partial class FP
-        {
-            public const string Asteroid = "res://scenes/Asteroid.tscn";
-        }
+	public static partial class Def
+	{
+		public static partial class FP
+		{
+			public const string Asteroid = "res://scenes/Asteroid.tscn";
+		}
 
-        public static partial class Player
-        {
-            public const float MoveSpeed = 0.1f;
-            public const float MaxMoveSpeed = 360f;
-            public const float RotationSpeed = 0.1f;
-            public const float MaxRotationSpeed = 4f;
-            public const float ShootDistance = 160f;
-        }
+		public static partial class Player
+		{
+			public const float MoveSpeed = 0.1f;
+			public const float MaxMoveSpeed = 360f;
+			public const float RotationSpeed = 0.1f;
+			public const float MaxRotationSpeed = 4f;
+			public const float ShootDistance = 160f;
+		}
 
-        public static partial class GameProcess
-        {
-            public const float AimDistance = 80f;
-        }
-    }
+		public static partial class GameProcess
+		{
+			public const float AimDistance = 80f;
+		}
+	}
 }
